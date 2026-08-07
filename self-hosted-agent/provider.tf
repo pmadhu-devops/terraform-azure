3df97ff7acbc83b80d6de9715868f36200b541e6
pmadhu-devops <panchakatlamadu1@gmail.com>
2026-08-07 10:10:27 +0530

    updated backend tf

--- self-hosted-agent/provider.tf
+++ self-hosted-agent/provider.tf
@@ -11,3 +11,14 @@ provider "azurerm" {
   features {}
   # subscription_id = "a7c4673c-e710-4514-b6e1-40cf4d73da90"
 }
+
+# Backend configuration for storing Terraform state in Azure Storage Account
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "devops-rg"
+    storage_account_name = "azuretfstorage01"
+    container_name       = "tfstate"
+    key                  = "azure.terraform.tfstate"
+  }
+}
+
